745c34efe95573506f361cccf58d8db48b544eb0
Marvin Helstein <17002425+KareVulli@users.noreply.github.com>
2019-02-11 15:25:52 +0200

    Most of the system done.

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
+/* jshint esversion: 6 */
 
-let clock = document.querySelector('#clock');
+let clockElement = document.querySelector('#clock');
+let dateElement = document.querySelector('#date');
 
 setInterval (updateTime, 1000);
 
 function updateTime() {
-    const date = new Date();
-    clock.innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getUTCSeconds();
+    clockElement.innerHTML = moment().format('HH:mm:ss');
+    dateElement.innerHTML = moment().format('dddd, DD.MM.YYYY');
 }
+
+document.getElementById('show-time').addEventListener('change', function() {
+    if (this.checked) {
+        clockElement.classList.remove('hidden');
+    } else {
+        clockElement.classList.add('hidden');
+    }
+});
+
+document.getElementById('show-date').addEventListener('change', function() {
+    if (this.checked) {
+        dateElement.classList.remove('hidden');
+    } else {
+        dateElement.classList.add('hidden');
+    }
+});
+
+document.getElementById('dark-theme').addEventListener('change', function() {
+    if (this.checked) {
+        document.body.classList.add('dark-theme');
+    } else {
+        document.body.classList.remove('dark-theme');
+    }
+});
+
+document.getElementById('time-size').addEventListener('change', function() {
+    let value = this.value;
+    if (value == 0) {
+        clockElement.classList.remove('clock-medium');
+        clockElement.classList.remove('clock-large');
+    } else if (value == 1) {
+        clockElement.classList.add('clock-medium');
+        clockElement.classList.remove('clock-large');
+    } else {
+        clockElement.classList.remove('clock-medium');
+        clockElement.classList.add('clock-large');
+    }
+});
 
 updateTime();
