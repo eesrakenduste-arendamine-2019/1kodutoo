924a1d64a88e121aa0166498af279d35d721413a
Priit Laupa <priilau@users.noreply.github.com>
2019-02-11 13:09:47 +0200

    Esimesed muudatused 1. kodutöös

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6*/
 let clockContainer;
+let dayContainer;
 
 window.onload = function(){
   init();
@@ -7,6 +8,7 @@ window.onload = function(){
 
 function init(){
   clockContainer = document.querySelector("#clockContainer");
+  dayContainerElement = document.querySelector("#dayContainer");
   startClock();
 }
 
@@ -20,4 +22,36 @@ function startClock(){
 function updateClock(){
   const date = new Date();
   clockContainer.innerHTML = date;
+  let day;
+  document.querySelector("#dayContainer");
+  function getDay(){
+    switch(new Date().getDay()){
+    default:
+      day = "See ei ole päev!";
+      break;
+    case 0:
+      day ="Pühapäev";
+      break;
+    case 1:
+      day = "Esmaspäev";
+      break;
+    case 2:
+      day = "Teisipäev";
+      break;
+    case 3:
+      day = "Kolmapäev";
+      break;
+    case 4:
+      day = "Neljapäev";
+      break;
+    case 5:
+      day = "Reede";
+      break;
+    case 6:
+      day = "Laupäev";
+      break;
+    }
   }
+  getDay();
+  dayContainerElement.innerHTML = day;
+}
