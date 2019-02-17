dacdec769e5578cc0c07f5d2ea55cbc95f70f0ba
kertliinat <43129546+kertliinat@users.noreply.github.com>
2019-02-17 16:54:27 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -3,34 +3,12 @@
 
 ## Tähtaeg 06.03.2018 23:59
 
-Kujunda kellanäide vastavalt maitsele või kindlale teemale, mahutades kella täisekraanile, et saaks kasutada lauakella või ekraanisäästja asemel. Selleks, et see sobiks paljudele ekraanidele, kasuta kujunduse loomisel protsendilisi väärtusi (nt width: 100%; ) või nt võimalda kella suurust kasutajal muuta.
+Kert Liinati esimene kodutöö
+Kasutajale on toodud välja nupud, millega saab muuta kirjalaadi, kirjalaadi värvust, taustavärvi
+ning loodud nupud öiseks ning päevaseks kasutamiseks, kus öine on veidi pimedama kujundusega ning päevane valik heledama kujundusega.
+Lisaks sellele kui kell on hiljem kui 20 ja varem kui 8 siis on automaatselt aktiveeritud öine režiim, kui kell on rohkem kui 8 ning vähem kui 20 on aktiveeritud pöevane režiim.
+Lisasin ka suuruse reguleerimise nupud, väärtused on minu enda poolt ette antud
+Ekraanitömmis on githubis eraldi failina, kuna ei tea kuidas .md faili pilti lisada. 
 
-## Nõuded
 
-1. Veebirakendus töötab. Näitab kella, kuupäeva ja nädalapäeva.
-1. Vastavalt kasutaja tegevusele on võimalik muuta **kolme** lauakella atribuuti muuta (nt suurus, värv, asukoht vms)
-1. Kell on originaalne ning kasutajaliides on maitsekalt kujundatud
-1. Autori ees- ja perenimi on lehel välja toodud
-1. Lehel on viide rakenduse repositooriumile
-1. Kood on vormistatud vastavalt stiilijuhendile
-1. `README.md` failis on välja toodud autori nimi, ekraanipilt rakendusest ja kirjeldatud funktsionaalsus
 
-## Mõned ideed võimalikeks täiendusteks (lihtsalt, et mõte hakkaks jooksma)
-
-* Vahetuda võivad taustapildid, taustal võib mängida muusika
-* Taustale klikkides muudetakse kella taustaväri
-* Kellale klõpsides muudetakse numbrite värvi või numbrite suurust;
-* Iga numbri suurust saab eraldi muuta
-* Nooleklahvidega saab kella ekraanil liigutada
-* Küsi enne kella näitamise alustamist kasutajalt tema lemmikvärv ja tee sellest lähtudes midagi
-* Muuda kella ja tausta värvi vastavalt ajale (päev/öö)
-* Kella suurus ja asukoht sõltuvad hiire asukohast ekraanil
-* Äkki võiks tegu olla hoopis analoogkellaga
-* Kirjatüüpi saab ka muuta, [google.com/fonts](https://www.google.com/fonts)
-* Kasutaja saab lisada või kasutajaöe näidatakse mitu kellaaega erinevatest maailma kohtadest
-
-## Abimaterjal
-
-* Sündmuste loetelu [HTML DOM Events](http://www.w3schools.com/jsref/dom_obj_event.asp)
-* Ajal põhinevad sündmused [JavaScript Timing Events](http://www.w3schools.com/js/js_timing.asp)
-* CSSi muutmine [HTML DOM Style Object](http://www.w3schools.com/jsref/dom_obj_style.asp)
--- index.js
+++ index.js
@@ -3,20 +3,139 @@ let clockContainer;
 let changeColorButton;
 let dayContainer;
 let day;
-
+let dateContainer;
+let nameContainer;
+let newFont = "Times New Roman";
 
 
 window.onload = function(){
   init();
+
+  document.getElementById("clockContainer").style.right = "40%";
+  document.getElementById("clockContainer").style.top = "40%";
+
+  document.getElementById("dayContainer").style.right = "45%";
+  document.getElementById("dayContainer").style.top = "50%";
+
+  document.getElementById("dateContainer").style.right = "39%";
+  document.getElementById("dateContainer").style.top = "55%";
+
+  /*document.getElementById("myBtn").style.top = "15%";
+  document.getElementById("myBtn").style.right = "94%";
+*/
+  document.getElementById("myColorBtn").style.top = "95%";
+
+  document.body.style.background = "#4db8ff";
+
+  nameContainer = document.querySelector('#nameContainer');
+  nameContainer.innerHTML= "Kert Liinat";
+
+  var date = new Date();
+
+  if(date.getHours() < 20 && date.getHours() >= 8){
+    dayMode();
+  }else if(date.getHours() >= 20 && date.getHours() < 8){
+    nightMode();
+  }
+
 };
 
+function returnSize(){
+  document.getElementById("clockContainer").style.fontSize = "500%";
+  document.getElementById("clockContainer").style.right = "40%";
+  document.getElementById("clockContainer").style.top = "40%";
+}
+
+function decreaseSize(){
+  document.getElementById("clockContainer").style.fontSize = "250%";
+  document.getElementById("clockContainer").style.right = "46%";
+  document.getElementById("clockContainer").style.top = "45%";
+
+}
+
+function nightMode(){
+  document.body.style.background = "#003366";
+  document.getElementById("clockContainer").style.color = "#ccccff";
+  document.getElementById("dayContainer").style.color = "#ccccff";
+  document.getElementById("dateContainer").style.color = "#ccccff";
+
+}
+
+function dayMode(){
+  document.body.style.background = "#ccffff";
+  document.getElementById("clockContainer").style.color = "#000033";
+  document.getElementById("dayContainer").style.color = "#000033";
+  document.getElementById("dateContainer").style.color = "#000033";
+}
+
+function randomizeBckColor(){
+  const r = Math.round(Math.random()*255);
+  const g = Math.round(Math.random()*255);
+  const b = Math.round(Math.random()*255);
+  document.body.style.backgroundColor = 'rgb('+ r+' , '+ g +' , '+ b +')';
+}
+
+function changeBckgColorToWhite(){
+  document.body.style.background = "White";
+}
+
+function originalBckColor(){
+  document.body.style.background = "#4db8ff";
+}
+
+function randomizeFontColor(){
+  const r = Math.round(Math.random()*255);
+  const g = Math.round(Math.random()*255);
+  const b = Math.round(Math.random()*255);
+  document.getElementById("clockContainer").style.color = 'rgb('+ r+' , '+ g +' , '+ b +')';
+  document.getElementById("dayContainer").style.color = 'rgb('+ r+' , '+ g +' , '+ b +')';
+  document.getElementById("dateContainer").style.color = 'rgb('+ r+' , '+ g +' , '+ b +')';
+
+}
+
+function changeFontColorToWhite(){
+  document.getElementById("clockContainer").style.color = "White";
+  document.getElementById("dayContainer").style.color = "White";
+  document.getElementById("dateContainer").style.color = "White";
+}
+
+function changeFontColorToBlack(){
+  document.getElementById("clockContainer").style.color = "Black";
+  document.getElementById("dayContainer").style.color = "Black";
+  document.getElementById("dateContainer").style.color = "Black";
+}
+function changeFontColorToRed(){
+  document.getElementById("clockContainer").style.color = "Red";
+  document.getElementById("dayContainer").style.color = "Red";
+  document.getElementById("dateContainer").style.color = "Red";
+}
+
+
+
+function defaultFont(){
+  document.getElementById("clockContainer").style.fontFamily = "Aldrich";
+  document.getElementById("dayContainer").style.fontFamily = "Aldrich";
+  document.getElementById("dateContainer").style.fontFamily = "Aldrich";
+}
+
+function changeFont(){
+  document.getElementById("clockContainer").style.fontFamily = newFont;
+  document.getElementById("dayContainer").style.fontFamily = newFont;
+  document.getElementById("dateContainer").style.fontFamily = newFont;
+}
+
+/*function changeSize(){
+  document.getElementById("clockContainer").style.width = "100px";
+}*/
+
 function init(){
-  changeColorButton = document.querySelector('#clockButton');
+  //changeColorButton = document.querySelector('#clockButton');
   clockContainer = document.querySelector('#clockContainer');
 
-  changeColorButton.addEventListener('click', changeBackgroundColor);
 
-  window.addEventListener('mousemove',changeBackgroundColor);
+  //changeColorButton.addEventListener('click', changeBackgroundColor);
+
+  //window.addEventListener('mousemove',changeBackgroundColor);
   //dayContainerElement =
   dayContainer = document.querySelector('#dayContainer');
   startClock();
@@ -33,20 +152,87 @@ function changeBackgroundColor(){
 }
 
 
+
+function pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+}
+
+
 function startClock(){
   updateClock();
   window.setInterval(function(){
-    const date = new Date();
-    clockContainer.innerHTML = date;
+    var date = new Date();
+    var HMS = pad(date.getHours()) +":" + pad(date.getMinutes())+":"+pad(date.getSeconds());
+    clockContainer.innerHTML = HMS;
   }, 1000);
 
 }
 function updateClock(){
-  const date = new Date();
-  clockContainer.innerHTML = date;
+  var date = new Date();
+  var HMS = pad(date.getHours()) +":" + pad(date.getMinutes())+":"+pad(date.getSeconds());
+  clockContainer.innerHTML = HMS;
   dayContainer = document.querySelector('#dayContainer');
+  dateContainer = document.querySelector('#dateContainer');
 
-  function getDay(){
+
+
+
+
+  function getM(){
+    switch(new Date().getMonth()){
+      case 0:
+        month= "Jaanuar";
+        console.log(month);
+        break;
+      case 1:
+        month= "Veebruar";
+        console.log(month);
+        break;
+      case 2:
+        month= "Märts";
+        console.log(month);
+        break;
+      case 3:
+        month= "Aprill";
+        console.log(month);
+        break;
+      case 4:
+        month= "Mai";
+        console.log(month);
+        break;
+      case 5:
+        month= "Juuni";
+        console.log(month);
+        break;
+      case 6:
+        month= "Juuli";
+        console.log(month);
+        break;
+      case 7:
+        month= "August";
+        console.log(month);
+        break;
+      case 8:
+        month= "September";
+        console.log(month);
+        break;
+      case 9:
+        month= "Oktoober";
+        console.log(month);
+        break;
+      case 10:
+        month= "November";
+        console.log(month);
+        break;
+      case 11:
+        month= "Detsember";
+        console.log(month);
+        break;
+
+
+    }
+  }
+function getDay(){
     switch(new Date().getDay()){
       case 0:
         day = "Pühapäev";
@@ -78,7 +264,9 @@ function updateClock(){
         break;
     }
   }
+  getM();
   getDay();
+dateContainer.innerHTML = date.getDate()+"."+month+" "+date.getFullYear();
 dayContainer.innerHTML = day;
 
 }
