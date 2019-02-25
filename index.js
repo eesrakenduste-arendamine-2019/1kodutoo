af94348be5f73917d6cd8999c59131b87120250d
Rait Väiko <43170837+raitvaiko@users.noreply.github.com>
2019-02-25 20:06:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-   
 	function decreaseFontSize() {
-        document.getElementById('day').style.fontSize = "15px";
+        	document.getElementById('day').style.fontSize = "15px";
 		document.getElementById('date').style.fontSize = "15px";
 		document.getElementById('time').style.fontSize = "55px";
-
-
     }
 	
 	function increaseFontSize() {
-        document.getElementById('day').style.fontSize = "35px";
+        	document.getElementById('day').style.fontSize = "35px";
 		document.getElementById('date').style.fontSize = "35px";
 		document.getElementById('time').style.fontSize = "75px";
     }
@@ -18,20 +15,18 @@
     function clock()
     {
 	  var d = new Date();
-	  
 	  var date = d.getDate();
-	  
+	    
 	  var month = d.getMonth();
 	  var montharr =["Jaanuar","Veebruar","Märts","Aprill","Mai","Juuni","Juuli","August","September","Oktoober","November","Detsember"];
 	  month=montharr[month];
-	  
-	  
+	    
 	  var day = d.getDay();
 	  var dayarr =["Pühapäev","Esmaspäev","Teisipäev","Kolmapäev","Neljapäev","Reede","Laupäev"];
 	  day=dayarr[day];
 	  
 	  var hour =d.getHours();
-      var min = d.getMinutes();
+      	  var min = d.getMinutes();
 	  var sec = d.getSeconds();
 	
 	  document.getElementById("day").innerHTML=day;
@@ -44,7 +39,6 @@
 function color() {
   document.body.style.background=document.getElementById('color').value;
 }
-
 
 window.onload=init;
 
