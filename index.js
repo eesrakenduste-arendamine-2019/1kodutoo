a00a2bd278121c3a68d8f07d344ed7f384b4361b
Gertin Pakkonen <gertpak@users.noreply.github.com>
2019-03-01 00:31:07 +0200

    Kodutöö tehtud

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /*jshint esversion:6*/
 
-let changeColorButton;
-let dayContainer;
-let timeContainer;
-let backgroundChange;
+let changeColorButton,backgroundChange;
+let dayContainer, timeContainer;
 let changeSize;
-let changeFontButton;
-let fontChanged;
+let changeFontButton, fontChanged;
+let change;
+let allHands, hourHand, minuteHand, secondHand;
+let changeClock;
 
 window.onload = function (){
   init();
@@ -15,21 +15,43 @@ window.onload = function (){
   timeContainer = document.querySelector('#timeContainer');
   changeColorButton = document.querySelector('#change-color');
   changeColorButton.addEventListener('click', changeBackgroundColor);
+  changeClockType = document.querySelector('#changeClockType');
+  changeClockType.addEventListener('click', changeClockT);
   changeFontButton = document.querySelector('#change-text-font');
   changeFontButton.addEventListener('click', changeTextFont);
-  fontChanged = 0;
+  fontChanged = 0; changeClock = 0;
 };
+function moveHands() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds/60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes/60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  const hour = now.getHours();
+  const hourDegrees = ((hour/12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
+  if(secondsDegrees === 90) {
+    allHands.forEach(hand => hand.style.transition = 'none') 
+  } else {
+    allHands.forEach(hand => hand.style.transition = '') 
+  }
+}
 function init(){
   startClock();
 }
 function startClock(){
-  updateClock();
-  window.setInterval(function(){
     updateClock();
-  }, 500);
+    window.setInterval(function(){
+      updateClock();
+    }, 500);
 }
+
+
 function updateClock() {
+  if(changeClock == 0) {
   const date = new Date();
   dayContainer = document.querySelector('#dayContainer');
   let day;
@@ -81,6 +103,9 @@ function updateClock() {
   let m = addZero(date.getMinutes());
   let s = addZero(date.getSeconds());
   time.innerHTML = h + ":" + m + ":" + s;
+  } else {
+    return;
+  }
 }
 
 function addZero(i) {
@@ -137,7 +162,7 @@ function changeTextFont(){
     fontChanged = 1;
     
   } else {
-    dayContainer.style.fontFamily = "Times New Roman";;
+    dayContainer.style.fontFamily = "Times New Roman";
     timeContainer.style.fontFamily = "Times New Roman";
     changeFontButton.innerHTML = "Arial";
     fontChanged = 0;
@@ -145,3 +170,65 @@ function changeTextFont(){
   }
 }
 
+function changeClockT() { 
+  if(changeClock == 0){
+    removeElement("timeContainer");
+    removeElement("dayContainer");
+    document.getElementById("change-text-font").disabled = true;
+    document.getElementById("textColorChange").disabled = true;
+    let clock = document.createElement("div");
+    clock.setAttribute('class', 'clock');
+    clock.setAttribute('id', 'clock');
+    document.querySelector('#container').appendChild(clock);
+
+    let clock_face = document.createElement("div");
+    clock_face.setAttribute('class', 'clock-face');
+    clock.appendChild(clock_face);
+    document.querySelector('.clock').appendChild(clock_face);
+
+    let backg = document.createElement("img");
+    backg.setAttribute('class', 'analogBack');  
+    backg.setAttribute('src', 'numberplate.png'); 
+
+    let hour_hand = document.createElement("div");
+    hour_hand.setAttribute('class', 'hand hour-hand');  
+
+    let minute_hand = document.createElement("div");
+    minute_hand.setAttribute('class', 'hand min-hand'); 
+
+    let second_hand = document.createElement("div");
+    second_hand.setAttribute('class', 'hand second-hand');
+
+    document.querySelector('.clock-face').appendChild(backg);
+    document.querySelector('.clock-face').appendChild(hour_hand);
+    document.querySelector('.clock-face').appendChild(minute_hand);
+    document.querySelector('.clock-face').appendChild(second_hand);
+    secondHand = document.querySelector('.second-hand');
+    minuteHand = document.querySelector('.min-hand');
+    hourHand = document.querySelector('.hour-hand');
+    allHands = document.querySelectorAll('.hand');
+    setInterval(moveHands, 1000);
+    changeClockType.innerHTML = "Digitaal kell";
+    changeClock = 1;
+  } else {
+    removeElement("clock");
+    let timeCont = document.createElement("div");
+    timeCont.setAttribute('class', 'timeContainer');
+    timeCont.setAttribute('id', 'timeContainer');
+    document.getElementById("change-text-font").disabled = false;
+    document.getElementById("textColorChange").disabled = false;
+    let dayCont = document.createElement("div");
+    dayCont.setAttribute('class', 'dayContainer');
+    dayCont.setAttribute('id', 'dayContainer');
+    document.querySelector('#container').appendChild(timeCont);
+    document.querySelector('#container').appendChild(dayCont);
+    dayContainer = document.querySelector('#dayContainer');
+    timeContainer = document.querySelector('#timeContainer');
+    changeClockType.innerHTML = "Analoog kell";
+    changeClock = 0;
+  }
+}
+function removeElement(elementId) {
+  let element = document.getElementById(elementId);
+  element.parentNode.removeChild(element);
+}
