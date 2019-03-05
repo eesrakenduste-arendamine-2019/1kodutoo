4b884114bd59f05f81e5b8138419f540a8300e67
Taavi Liivat <43164710+taavlii@users.noreply.github.com>
2019-03-05 23:52:57 +0200

    esimene vers, vaatame kuidas pildi link readmes toimib

--- index.js
+++ index.js
@@ -1,104 +1,125 @@
 /*jshint esversion: 6 */
-let clockContainer;
-let changeColor;
 
 
 window.onload = function(){
   init();
-}
+
+};
+
 
 
 
 function init(){
-changeColorButton=document.querySelector('#clockButton');
-clockContainer=document.querySelector('#clockContainer');
-dayContainer=document.querySelector('#dayContainer');
-
-//changeColorButton.addEventListener('click', changeBackgroundColor);
-window.addEventListener('keypress', changeBackgroundColor);
-
-  startClock();
+  startTime();
+  getDay();
 }
-
-
 
 function getDay(){
-
-}
-
-function changeBackgroundColor(){
-  console.log("muuda v2rv");
-  const r=Math.round(Math.random()*255);
-  const g=Math.round(Math.random()*255);
-  const b=Math.round(Math.random()*255);
-  clockContainer.style.backgroundColor='rgb('+r+','+g+','+b+')';
-  document.body.style.backgroundColor='rgb('+r+','+g+','+b+')';
-}
-
-function startClock(){
-  window.setInterval(function(){
-    updateClock();
-
-  },1000)
-  ;
-}
-
-function updateClock(){
   const date=new Date();
-  clockContainer.innerHTML = date;
   let day;
-  dayContainerElement=document.querySelector('#dayContainer');
-  function getDay(){
-    switch(date.getDay()){
-      case 0:
-          day="Pühapäev";
-          //console.log(day);
-          break;
-      case 1:
-          day="Esmaspäev";
-          //console.log(day);
-          break;
-      case 2:
-          day="Teisipäev";
-          //console.log(day);
-          break;
-      case 3:
-          day="Kolmapäev";
-          //console.log(day);
-          break;
-      case 4:
-          day="Neljapäev";
-          //console.log(day);
-          break;
-      case 5:
-          day="Reede";
-          //console.log(day);
-          break;
-      case 6:
-          day="Laupäev";
-          //console.log(day);
-          break;
-      case 7:
-          day="Pühapäev";
-          //console.log(day);
-          break;
-      case default:
-          day="Kes teab";
-          //console.log(day);
-          break;
-    }
+  switch(date.getDay()){
+    case 0:
+        day="Pühapäev";
+        //console.log(day);
+        break;
+    case 1:
+        day="Esmaspäev";
+        //console.log(day);
+        break;
+    case 2:
+        day="Teisipäev";
+        //console.log(day);
+        break;
+    case 3:
+        day="Kolmapäev";
+        //console.log(day);
+        break;
+    case 4:
+        day="Neljapäev";
+        //console.log(day);
+        break;
+    case 5:
+        day="Reede";
+        //console.log(day);
+        break;
+    case 6:
+        day="Laupäev";
+        //console.log(day);
+        break;
   }
-  getDay();
-  dayContainerElement.innerHTML = day;
+  let monthDay;
+  monthDay=checkTime((date.getDate()));
+  let month;
+  month=checkTime((date.getMonth()+1));
+
+
+  document.getElementById('day').innerHTML=day+", "+monthDay+"."+month;
 }
 
 
-/*function doMath(){
-  var short=document.getElementById("math");
-  //short.innerHTML = Math.round(4.4);
-  //short.innerHTML = Math.ceil(4.1);
-  //short.innerHTML = Math.floor(4.8);
-  //short.innerHTML = Math.pow(4,3);
-  short.innerHTML = Math.round(Math.random()*100);
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
-*/
+
+function startTime() {
+  var secondCheckbox=document.getElementById("secondSelect");
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  s = checkTime(s);
+  if (secondCheckbox.checked == true){
+   document.getElementById('time').innerHTML = h + ":" + m;
+ } else {
+   document.getElementById('time').innerHTML = h + ":" + m+":"+s;
+ }
+  t = setTimeout(function() {
+    startTime();
+  }, 500);
+}
+
+function clockSize(){
+  var sizeCheckbox=document.getElementById("superSize");
+  if(sizeCheckbox.checked == true){
+    document.getElementById('time').style.fontSize="3700%";
+  }else{
+    document.getElementById('time').style.fontSize="700%";
+  }
+}
+
+function colorScheme(){
+  var colorBox=document.getElementById("darkMode");
+  if(colorBox.checked==true){
+    document.body.style.backgroundColor="#000000";
+    document.body.style.color="#1a1a1a";
+  }else{
+    document.body.style.backgroundColor="#00CACA";
+    document.body.style.color="#000000";
+  }
+}
+
+function  rndCol(){
+  const r = Math.round(Math.random()*255);
+  const g = Math.round(Math.random()*255);
+  const b = Math.round(Math.random()*255);
+  document.getElementById("creator").style.color = "rgb("+r+","+g+","+b+")";
+  t = setTimeout(function() {
+    rndCol();
+  }, 80);
+}
+
+
+function showCreator(){
+  rndCol();
+  var infoBox=document.getElementById("showCreator");
+  if(infoBox.checked == true){
+    document.getElementById('creator').innerHTML="Taavi Liivat";
+  }else{
+    document.getElementById('creator').innerHTML="";
+  }
+}
