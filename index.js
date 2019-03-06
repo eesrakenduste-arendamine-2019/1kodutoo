623accfa60b7eb0776aa52749420baf41b262dc3
Lauri Raus <laurrau@users.noreply.github.com>
2019-03-06 17:35:14 +0200

    Kodutoo Lauri Raus

--- index.js
+++ index.js
@@ -1,108 +1,52 @@
-let clockContainer;
+/*jshint esversion: 6*/
+window.onload = setInterval(clock,1000);
 let changeColorButton;
-let changeTextColorButton
-let clockPos
-let datePos
-let dayPos
-let buttonsPos
+let changeTextButton;
 
-window.onload = function(){
+window.onload = function () {
   init();
-  startTime();
 };
 
-function init(){
-	changeColorButton = document.querySelector('#clockButton');
-  clockContainer = document.querySelector("#clockContainer");
-	dayContainerElement = document.querySelector('#dayContainer');
-	//changeColorButton.addEventListener('click' , changeBackgroundColor);
-  startClock();
-	changeColorButton.addEventListener('click', changeBackgroundColor);
-	changeTextColorButton = document.querySelector('#textColorButton');
-	changeTextColorButton.addEventListener('click', changeTextColor);
+function init() {
+	 changeColorButton = document.querySelector('#colorButtonBackground');
+	  changeTextButton = document.querySelector('#textColorButton');
+	 changeColorButton.addEventListener('click', changeBackgroundColor);
+	 changeTextButton.addEventListener('click', changeTextColor)
 }
 
-clockPos = document.getElementById("clockPos");
-
-function changeBackgroundColor(){
-	console.log("Muudan värvi");
-	const r = Math.round(Math.random()*255);
-	const g = Math.round(Math.random()*255);
-	const b = Math.round(Math.random()*255);
-
-	//clockContainer.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
-	document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+function changeBackgroundColor() {
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  document.body.style.background = `rgb(${r}, ${g}, ${b})`;
 }
 
-function changeTextColor(){
-	const r = Math.round(Math.random()*255);
-	const g = Math.round(Math.random()*255);
-	const b = Math.round(Math.random()*255);
-
-	document.body.style.color = `rgb(${r}, ${g}, ${b})`;
+function changeTextColor() {
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  document.body.style.color = `rgb(${r}, ${g}, ${b})`;
 }
-
-function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('txt').innerHTML =
-  h + ":" + m + ":" + s;
-  var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-  return i;
-}
-
-function startClock(){
-  updateClock();
-  window.setInterval(function(){
-    updateClock();
-  }, 1000);
-}
-
-function updateClock(){
-  const date = new Date();
-  clockContainer.innerHTML = date;
-	let day;
-	function getDay(){
-		switch(new Date().getDay()){
-			case 0:
-			day = "Pühapäev";
-			console.log(day);
-			break;
-			case 1:
-			day = "Esmaspäev";
-				console.log(day);
-			break;
-			case 2:
-			day = "Teisipäev";
-				console.log(day);
-			break;
-			case 3:
-			day = "Kolmapäev";
-				console.log(day);
-			break;
-			case 4:
-			day = "Neljapäev";
-				console.log(day);
-			break;
-			case 5:
-			day = "Reede";
-				console.log(day);
-			break;
-			case 6:
-			day = "Laupäev";
-				console.log(day);
-			break;
-			default:
-			day ="See pole päev";
-		}
-	}
-	getDay();
-	dayContainerElement.innerHTML = day;
+//http://talkerscode.com/webtricks/digital-clock-with-complete-time-and-date-using-css-and-javascript.php Kella funksioon
+function clock() {
+	  var d = new Date();
+	  
+	  var date = d.getDate();
+	  
+	  var month = d.getMonth();
+	  var months =["Jaanuar","Veebruar","Märts","Aprill","Mai","Juuni","Juuli","August","September","Oktoober","November","Detsember"];
+	  month=months[month];
+	  
+	  var year = d.getFullYear();
+	  
+	  var day = d.getDay();
+	  var days =["Pühapäev","Esmaspäev","Teisipäev","Kolmapäev","Neljapäev","Reede","Laupäev"];
+	  day=days[day];
+	  
+	  var hour =d.getHours();
+      var min = d.getMinutes();
+	  var sec = d.getSeconds();
+	
+	  document.getElementById("date").innerHTML=day+" "+date+" "+month+" "+year;
+	  document.getElementById("time").innerHTML=hour+":"+min+":"+sec;
 }
