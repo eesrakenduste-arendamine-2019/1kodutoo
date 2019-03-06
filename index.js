5e2d7eb5f93e37fae0af84e3b8b7beea01404615
Rait Väiko <43170837+raitvaiko@users.noreply.github.com>
2019-03-06 21:48:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-	function decreaseFontSize() {
-        	document.getElementById('day').style.fontSize = "15px";
-		document.getElementById('date').style.fontSize = "15px";
-		document.getElementById('time').style.fontSize = "55px";
-    }
+/*jshint esversion:6*/
+	function original() {
+		document.getElementById('day').style.fontSize = "25px";
+		document.getElementById('date').style.fontSize = "25px";
+		document.getElementById('time').style.fontSize = "65px";
 	
-	function increaseFontSize() {
-        	document.getElementById('day').style.fontSize = "35px";
-		document.getElementById('date').style.fontSize = "35px";
-		document.getElementById('time').style.fontSize = "75px";
+
     }
-   
+
+	function increaseFontSize() {
+		document.getElementById('day').style.fontSize = "45px";
+		document.getElementById('date').style.fontSize = "45px";
+		document.getElementById('time').style.fontSize = "85px";
+    }
+
 	window.onload = setInterval(clock,1000);
 
     function clock()
     {
 	  var d = new Date();
+
+
 	  var date = d.getDate();
-	    
+
 	  var month = d.getMonth();
 	  var montharr =["Jaanuar","Veebruar","Märts","Aprill","Mai","Juuni","Juuli","August","September","Oktoober","November","Detsember"];
 	  month=montharr[month];
-	    
+
+
 	  var day = d.getDay();
 	  var dayarr =["Pühapäev","Esmaspäev","Teisipäev","Kolmapäev","Neljapäev","Reede","Laupäev"];
 	  day=dayarr[day];
-	  
+
 	  var hour =d.getHours();
-      	  var min = d.getMinutes();
+          var min = d.getMinutes();
 	  var sec = d.getSeconds();
-	
+
 	  document.getElementById("day").innerHTML=day;
 	  document.getElementById("date").innerHTML=date+" "+month;
 	  document.getElementById("time").innerHTML=hour+":"+min+":"+sec;
+	  document.getElementById('time').onclick = changeColor; 
 
     }
 
@@ -40,8 +47,19 @@ function color() {
   document.body.style.background=document.getElementById('color').value;
 }
 
+
+function changeColor() {
+  document.getElementById("time").style.color = "#ff0000";
+  document.getElementById("date").style.color = "#ff0000";
+  document.getElementById("day").style.color = "#ff0000";
+}
+
+function changeColor1() {
+  document.getElementById("time").style.color = "blue";
+  document.getElementById("date").style.color = "blue";
+  document.getElementById("day").style.color = "blue";
+}
+
+
 window.onload=init;
-
-
-
 
