0290fa87bb2fa6fcacf15da4fe2b93c67a35fcdf
raimop <43129557+raimop@users.noreply.github.com>
2019-03-13 22:46:27 +0200

    Add files via upload

--- clock1.js
+++ clock1.js
@@ -11,6 +11,17 @@ let test;
 
 window.onload = function(){
   clockInfo();
+  
+  let containerCss = document.querySelector('.container');
+  let bodyCss = document.querySelector('body');
+  
+  containerCss.style.marginTop = "30vh";
+  containerCss.style.fontSize = "80px";
+  containerCss.style.color = "white";
+  containerCss.style.textShadow = "3px 3px 1px yellow, 6px 6px 1px orange";
+  bodyCss.style.backgroundColor = "black";
+  bodyCss.style.cursor = "progress";
+  
   window.setInterval(function(){
     clockInfo();
   }, 100);
@@ -43,13 +54,11 @@ function clockInfo(){
     year = date.getFullYear();
 
     clock = document.querySelector('#clock');
-    /*document.querySelector('#hours').innerHTML = "<p onclick=\'changeFontSize(\"#hours\",10)\'>+</p>" + "<p onclick=\'changeFontSize(\"#hours\",-10)\'>-</p>" + hours;
-    document.querySelector('#minutes').innerHTML = "<p onclick=\'changeFontSize(\"#minutes\",10)\'>+</p>" + "<p onclick=\'changeFontSize(\"#minutes\",-10)\'>-</p>" + minutes;
-    document.querySelector('#seconds').innerHTML = "<p onclick=\'changeFontSize(\"#seconds\",10)\'>+</p>" + "<p onclick=\'changeFontSize(\"#seconds\",-10)\'>-</p>" + seconds;*/
-
 
     document.querySelector('#hours').innerHTML = hours;
+	document.querySelector('#hoursminutes').innerHTML = ":";
     document.querySelector('#minutes').innerHTML = minutes;
+	document.querySelector('#minutesseconds').innerHTML = ":";
     document.querySelector('#seconds').innerHTML = seconds;
 
     document.querySelector('#day').innerHTML = weekDays[actualDay];
@@ -60,8 +69,29 @@ function clockInfo(){
 
     document.querySelector('#date').innerHTML = dayOfMonth + ". " + months[month] + " " + year;
 
-    //hoursDiv.innerHTML = hours;
-    //clock.innerHTML = "hours + <br>" + minutes + "<br>" + seconds + "<br> Päev: " + weekDays[actualDay] + " " + dayOfMonth;
+}
+
+function nextElement(array, element, index){
+	return array[(array.indexOf(element) + index) % array.length];
+}
+
+function calculateSemiColon(input1, input2, input3){
+	let one = document.querySelector(input1).style.fontSize;
+	let two = document.querySelector(input2).style.fontSize;
+	
+	let check = document.querySelector('.container');
+	
+	if(!two && check){
+		two = check.style.fontSize;
+	}
+	if (!one && check){
+		one = check.style.fontSize;
+	}
+	
+	let three = ((parseInt(two) + parseInt(one))/2);
+	
+	document.querySelector(input3).style.fontSize = three.toString() + "px";
+	
 }
 
 function changeFontSize(what, howMuch){
@@ -75,6 +105,7 @@ function changeFontSize(what, howMuch){
       base.style.fontSize = "10px";
     }
   }
+  
   var oldStyle = parseInt(base.style.fontSize);
   oldStyle = oldStyle + howMuch;
   if (oldStyle <= 0){
@@ -83,24 +114,37 @@ function changeFontSize(what, howMuch){
     oldStyle = 390;
   }
   var newStyle = base.style.fontSize = oldStyle.toString() + "px";
+  
+  
+  if (what == "#hours"){
+	  calculateSemiColon("#hours", "#minutes", "#hoursminutes");
+  } else if (what == "#minutes"){
+	  calculateSemiColon("#hours", "#minutes", "#hoursminutes");
+	  calculateSemiColon("#minutes", "#seconds", "#minutesseconds");
+  } else if (what == "#seconds"){
+	  calculateSemiColon("#minutes", "#seconds", "#minutesseconds");
+  }
 
 }
 
 function clockUpDown(howMuch){
   let base = document.querySelector('.container');
   if (base.style.marginTop == ""){
-    base.style.marginTop = "0%";
+    base.style.marginTop = "0vh";
   }
+  console.log(base.style.marginTop);
 
   var oldStyle = parseInt(base.style.marginTop);
+  console.log(oldStyle);
   oldStyle += howMuch;
+  console.log(oldStyle);
   if (oldStyle <= 0){
     oldStyle = 0;
-  } else if (oldStyle >= 40){
-    oldStyle = 40;
+  } else if (oldStyle >= 90){
+    oldStyle = 90;
   }
 
-  var newStyle = base.style.marginTop = oldStyle.toString() + "%";
+  var newStyle = base.style.marginTop = oldStyle.toString() + "vh";
 }
 
 function clockLeftRight(howMuch){
@@ -122,59 +166,53 @@ function changeFont(){
   }
 
   let fontMassif = ["Arial", "Calibri", "Cambria", "Impact", "Ebrima", "Marlett", "SimSun", "Sylfaen", "Symbol"];
-
-  for (let i = 0; i < fontMassif.length; i++){
-    if (fontMassif[i] == base.style.fontFamily){
-      if (i == (fontMassif.length-1)){
-        base.style.fontFamily = fontMassif[0];
-        break;
-      } else {
-        base.style.fontFamily = fontMassif[i+1];
-        break;
-      }
-    }
-  }
+  
+  base.style.fontFamily = nextElement(fontMassif, base.style.fontFamily, 1);
 }
 
 function changeColour(){
   let base = document.querySelector('.container');
-  if (base.style.color == ""){
-    base.style.color = "black";
-  }
+  let body = document.querySelector('body');
 
-  let fontMassif = ["black", "white", "purple", "yellow", "violet", "hotpink", "blue", "cyan", "grey", "green"];
+  let colorMassif = ["black", "white", "purple", "yellow", "violet", "hotpink", "blue", "cyan", "grey", "green"];
 
-  for (let i = 0; i < fontMassif.length; i++){
-    if (fontMassif[i] == base.style.color){
-      if (i == (fontMassif.length-1)){
-        base.style.color = fontMassif[0];
-        break;
-      } else {
-        base.style.color = fontMassif[i+1];
-        break;
-      }
-    }
-  }
+  let index = 1;
+    
+  if (nextElement(colorMassif, base.style.color, index) == nextElement(colorMassif, body.style.backgroundColor, index-1)){
+	  index = 2;
+  } 
+  
+  base.style.color = nextElement(colorMassif, base.style.color, index);
 }
 
 function changeBackgroundColour(){
-  let base = document.querySelector('body');
-  if (!base.style.backgroundColor){
-    base.style.backgroundColor = "white";
-  }
+	let base = document.querySelector('.container');
+	let body = document.querySelector('body');
+  
+	let colorMassif = ["white", "black", "purple", "yellow", "violet", "hotpink", "blue", "cyan", "grey", "green"];
 
-  let fontMassif = ["white", "black", "purple", "yellow", "violet", "hotpink", "blue", "cyan", "grey", "green"];
+	let index = 1;
+    
+	if (nextElement(colorMassif, body.style.backgroundColor, index) == nextElement(colorMassif, base.style.color, index-1)){
+	  index = 2;
+	} 
 
-  for (let i = 0; i < fontMassif.length; i++){
-    if (fontMassif[i] == base.style.backgroundColor){
-      console.log(fontMassif[i]);
-      if (i == (fontMassif.length-1)){
-        base.style.backgroundColor = fontMassif[0];
-        break;
-      } else {
-        base.style.backgroundColor = fontMassif[i+1];
-        break;
-      }
-    }
-  }
+	body.style.backgroundColor = nextElement(colorMassif, body.style.backgroundColor, index);
+  
+}
+
+function changeShadow(){
+	let base = document.querySelector('.container');
+	
+	let colorMassif = ["purple 3px 3px 1px, yellow 6px 6px 1px", "yellow 3px 3px 1px, orange 6px 6px 1px", "orange 3px 3px 1px, red 6px 6px 1px", "red 3px 3px 1px, purple 6px 6px 1px"];
+	
+	base.style.textShadow = nextElement(colorMassif, base.style.textShadow, 1);
+}
+
+function changeCursor(){
+	let base = document.querySelector('body');
+	
+	let cursorMassif = ["all-scroll", "crosshair", "help", "not-allowed", "text", "wait", "zoom-in", "zoom-out", "progress"];
+	
+	base.style.cursor = nextElement(cursorMassif, base.style.cursor, 1);
 }
